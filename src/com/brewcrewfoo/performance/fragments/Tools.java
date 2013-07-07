662bbf7d5eb50ac5cb7301f504c349e4c452dd25
h0rn3t <horn3t69@gmail.com>
2013-07-07 16:26:28 +0300

    Create Tools.java

---
+++ src/com/brewcrewfoo/performance/fragments/Tools.java
@@ -0,0 +1,77 @@
+/*
+ * Performance Control - An Android CPU Control application Copyright (C) 2012
+ * James Roberts
+ * 
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.brewcrewfoo.performance.fragments;
+
+import android.app.AlertDialog;
+import android.app.Fragment;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.preference.PreferenceManager;
+import android.view.*;
+import android.widget.*;
+import android.widget.AdapterView.OnItemSelectedListener;
+import com.brewcrewfoo.performance.R;
+import com.brewcrewfoo.performance.activities.PCSettings;
+import com.brewcrewfoo.performance.util.CMDProcessor;
+import com.brewcrewfoo.performance.util.Constants;
+import com.brewcrewfoo.performance.util.Helpers;
+
+import java.io.File;
+
+
+public class Tools extends Fragment implements
+Constants {
+
+    private SharedPreferences mPreferences;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+  	mPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.tools_menu, menu);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.app_settings) {
+            Intent intent = new Intent(getActivity(), PCSettings.class);
+            startActivity(intent);
+        }
+        return true;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup root,Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.tools, root, false);
+
+        return view;
+    }
+
+
+
+
+}
